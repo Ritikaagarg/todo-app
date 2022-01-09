e6c9e862ee9f9ab6812e4e31385c084709c969b6
Ritika Garg <ritikagarg@Ritikas-MacBook-Air.local>
2022-01-09 14:59:49 +0530

    feat: add google signin auth

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import {Button} from 'react-native-paper';
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+
+const AuthScreen = () => {
+
+  const onSignIn = async () => {
+    try{
+      await GoogleSignin.hasPlayServices();
+      const {idToken} = await GoogleSignin.signIn();
+      console.log("token", idToken)
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      console.log("credential", googleCredential);
+      console.log("sign in", auth().signInWithCredential(googleCredential))
+      return auth().signInWithCredential(googleCredential);
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  React.useEffect(()=> {
+    GoogleSignin.configure({
+      webClientId: "13142218364-mh8436k2e8raib8p636bklq1l7paemrg.apps.googleusercontent.com",
+      offlineAccess: true,
+    });
+  },[])
+  return(
+    <View>
+      <Button onPress={onSignIn} >Login with Google</Button>
+    </View>
+  )
+}
+
+export default AuthScreen;
